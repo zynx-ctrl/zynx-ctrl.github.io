39d877b7eba76f65be0df62163c9ceeb0a8a80f2
ZYNX <narekandhrachya@gmail.com>
2025-03-18 13:30:59 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Smooth Scroll for Navigation Links
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
